a8ce8ebd27c73fab8bd4ccbac9fe48315741c8cd
Matthew Heseltine <106039469+mattboombox@users.noreply.github.com>
2024-10-19 18:07:27 -0700

    Added player class

--- Newt/NewtMain.py
+++ Newt/NewtMain.py
@@ -1,4 +1,5 @@
 import pygame
+import player.py
 
 pygame.init()
 
@@ -8,3 +9,18 @@ screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Newt")
+
+r = 20
+g = 50
+b = 200
+color = (r,g,b)
+
+running = True
+while(running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running  = False
+
+    screen.fill(color)
+    pygame.display.flip()
+    
---
+++ player.py
@@ -0,0 +1,11 @@
+import pygame
+
+class player:
+
+    def __init__(self, size, speed, color, hp, ad):
+        self.size = size
+        self.speed = speed
+        self.color = color
+        self.hp = hp
+        self.ad = ad
+    
