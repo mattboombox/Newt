58a4281735642d5ce838a15330195f83f6928bff
Matthew Heseltine <106039469+mattboombox@users.noreply.github.com>
2024-10-19 17:38:28 -0700

    Add files via upload
    Added NewtMain

---
+++ Newt/NewtMain.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+print("Hello")
+
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Newt")
