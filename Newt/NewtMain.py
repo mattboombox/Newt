b90c72dc11c4136f283a7680ec9b8f5197f0043c
Matthew Heseltine <106039469+mattboombox@users.noreply.github.com>
2024-10-22 19:23:52 -0700

    Update NewtMain.py

--- Newt/NewtMain.py
+++ Newt/NewtMain.py
@@ -5,17 +5,18 @@ pygame.init()
 
 print("Hello")
 
+color = (40,69,165)
+
 screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Newt")
 
-r = 20
-g = 50
-b = 200
-color = (r,g,b)
+#Game board
+board = [['X' for _ in range(10)] for _ in range(10)]
+board[0][0] = 'Y'
+print(board)
 
-sand = tile.tile(10,(100,200,30),10,10)
 
 running = True
 while(running):
@@ -23,13 +24,7 @@ while(running):
         if event.type == pygame.QUIT:
             running  = False
 
-    sand.posx = sand.posx + 0.1
-
-    if sand.posx > 1:
-        sand.posy =sand.posy + .01
-
 
     screen.fill(color)
-    pygame.draw.rect(screen,(sand.color),(sand.size,sand.size,sand.posx,sand.posy))
     pygame.display.flip()
     
