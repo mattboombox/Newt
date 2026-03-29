f33ffb0a83e1c3482ccd5d09a7f7a1641b2331a9
Matthew Heseltine <matthew.heseltine@wsu.edu>
2026-03-29 12:01:12 -0700

    fixed north pole generation

--- Newt/erosion.py
+++ Newt/erosion.py
@@ -91,7 +91,7 @@ def apply_polar_climate(world):
             if tile is None:
                 continue
 
-            if tile.terrain == ("ocean", "lake"):
+            if tile.terrain in ("ocean", "lake"):
                 tile.set_terrain("ice_sheet")
                 changed = True
             elif tile.terrain not in ("ice_sheet","mountain", "active_volcano", "dormant_volcano", "lava"):
